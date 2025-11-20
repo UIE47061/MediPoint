5efa9d6653056920181e9edf85a14568ea64a1f3
UIE47061 <ue4761@gmail.com>
2025-11-20 22:37:16 +0800

    [modify] 調整base_api路徑

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,10 +5,7 @@ import axios from 'axios'
 // ==========================================
 // ⚡️ 設定 API 連線位置
 // ==========================================
-// 如果你在本機跑後端，請用 'http://127.0.0.1:8000'
-// 如果要連 Hugging Face，請填入 Direct URL，例如 'https://uie47061-medipoint-backend.hf.space'
-const API_BASE_URL = 'https://uie47061-medipoint.hf.space/' 
-// const API_BASE_URL = 'https://你的HuggingFace網址.hf.space' 
+const API_BASE_URL = 'https://uie47061-medipoint.hf.space' 
 
 // --- 狀態管理 ---
 const loading = ref(true)
